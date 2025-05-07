9ae95b80818a1cc141e4654bb3e87fe51056605f
Noma <philliswadin@gmail.com>
2025-05-07 19:28:15 +0200

    added a class for the env
    init method initializes the 8x8 grid and the agent start position. move method defines possible actions and checks if agent has not hit a wall. Render method used to visualize on console

--- grid.py
+++ grid.py
@@ -1,5 +1,50 @@
 import numpy as np
-#defining a 8x8 grid
+
+class GridWorldEnv:
+    def __init__(self,grid):
+        pass
+        self.grid = grid
+        self.height, self.width = grid.shape
+        self.start_pos = grid[0][0]
+        self.goal_pos = grid[7][4]
+        self.agent_pos = self.start_pos
+        
+
+def move(self, direction,grid):
+    new_x, new_y = self.x, self.y
+    if direction == "up":
+        new_x -= 1
+    elif direction == "down":
+        new_x += 1
+    elif direction == "right":
+        new_y += 1
+    elif direction == "left":
+        new_y -= 1
+
+    if 0<= new_x < self.height and 0 <= new_y < self.width:
+        if grid[new_x][new_y] != 'w':
+            self.agent_pos = (new_x, new_y)
+        else:
+            print("Hit a wall")
+
+def render(self):
+    for i in range(self.height):
+        row = ''
+        for j in range(self.width):
+            if(i, j)== self.agent_pos:
+                row += 'A'
+            elif self.grid[i, j] =='w':
+                row += 'X'
+            elif self.grid[i, j] == 'g':
+                row += 'G'
+            elif self.grid[i, j] == 's':
+                row += 'S'
+            else:
+                row += ' . '
+        print(row)
+    print()
+
+
 grid = np.array ([
                   ['s','w',0,0,0,0,0,0],
                   [0,0,0,0,0,'w',0,'w'],
@@ -10,3 +55,11 @@ grid = np.array ([
                   ['w','w','w',0,'w','w','w',0],
                   [0,0,0,0,0,0,0,0]
                   ])
+
+env = GridWorldEnv(grid)
+
+env.render()
+
+env.step('right')
+env.step('down')
+env.render()
