a22e0b611eabe0d52c2f0b735025c680b4c43b4e
Noma <philliswadin@gmail.com>
2025-05-07 19:32:59 +0200

    fixed some errors and changed indentation

--- grid.py
+++ grid.py
@@ -5,44 +5,44 @@ class GridWorldEnv:
         pass
         self.grid = grid
         self.height, self.width = grid.shape
-        self.start_pos = grid[0][0]
-        self.goal_pos = grid[7][4]
+        self.start_pos = (0,0)
+        self.goal_pos = (7,4)
         self.agent_pos = self.start_pos
         
 
-def move(self, direction,grid):
-    new_x, new_y = self.x, self.y
-    if direction == "up":
-        new_x -= 1
-    elif direction == "down":
-        new_x += 1
-    elif direction == "right":
-        new_y += 1
-    elif direction == "left":
-        new_y -= 1
+    def move(self, direction,grid):
+        new_x, new_y = self.agent_pos
+        if direction == "up":
+            new_x -= 1
+        elif direction == "down":
+            new_x += 1
+        elif direction == "right":
+            new_y += 1
+        elif direction == "left":
+            new_y -= 1
 
-    if 0<= new_x < self.height and 0 <= new_y < self.width:
-        if grid[new_x][new_y] != 'w':
-            self.agent_pos = (new_x, new_y)
+        if 0<= new_x < self.height and 0 <= new_y < self.width:
+            if grid[new_x, new_y] != 'w':
+                self.agent_pos = (new_x, new_y)
         else:
             print("Hit a wall")
 
-def render(self):
-    for i in range(self.height):
-        row = ''
-        for j in range(self.width):
-            if(i, j)== self.agent_pos:
-                row += 'A'
-            elif self.grid[i, j] =='w':
-                row += 'X'
-            elif self.grid[i, j] == 'g':
-                row += 'G'
-            elif self.grid[i, j] == 's':
-                row += 'S'
-            else:
-                row += ' . '
-        print(row)
-    print()
+    def render(self):
+        for i in range(self.height):
+            row = ''
+            for j in range(self.width):
+                if(i, j)== self.agent_pos:
+                    row += 'A'
+                elif self.grid[i, j] =='w':
+                    row += 'X'
+                elif self.grid[i, j] == 'g':
+                    row += 'G'
+                elif self.grid[i, j] == 's':
+                    row += 'S'
+                else:
+                    row += ' . '
+            print(row)
+        print()
 
 
 grid = np.array ([
